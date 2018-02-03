866fe51e3f5c95c40e163aa0d8b9b5019ee5452d
hiturarael <hiturarael@hotmail.com>
2018-02-03 13:46:06 -0500

    index

--- index.php
+++ index.php
@@ -1,1 +1,44 @@
 <?php
+?>
+
+<html>
+	<head>
+		<title>Divine Blood</title>
+	</head>
+	
+	<body>
+		<header>Divine Blood</header>
+		
+		<div>
+			<p class="description">
+				In an alternate universe, in the ancient past, the Gods and Demons beheld the mortal realm. They larned of humanity, and begain to live amongst them, guiding them. And watching, to see what Humanity would become. 
+
+				In this world, during World War 2, Douglas McArthur turned left instead of right. Now, Burma arises as a threat, the Soviet Union has collapsed into a series of warring states. 
+
+				In this world, species that have grown parallel to humanity watch secretly, and try to live normal lives. But they cannot stay secret forever.
+
+
+				In this world, psychics are growing in power, and more and more respected. While many doubt, scientific proof of their ability has been found, and they are beginning to gain recognition. They are growing more and more powerful, and no one quite knows when it will stop. 
+
+
+				In this world, a single mother, raising three kids, investigates a disappearance of a tourist. She has found what she considers a safe neighborhood. Though her definition of safe is...rather different from what people think.
+
+
+    			All these stories have come together, to collide. Even if they don't know who the others are. Humanity is evolving, and no one knows what it will become. A pity evolution can be...inconvent.
+		   </p>
+		   
+		   <p class="description">
+			   Divine Blood is an Urban Fantasy novel by Luke Green, alias Thrythlind, which takes place in a world that diverged from our own. As with many Urban Fantasy worlds, monsters and beasts not known to humanity live amongst us. However, there are a few unique traits that seperate it from the works of Jim Butcher, or others. In most other works, the Supernatural races are just BETTER then humans. THough, of course, a well-trained human can defeat a being above his weight class, where the tension resides. In Divine Blood, supernatural beings are often civilians themselves. They themselves have to be 'well trained' to be considered people ready to fight in large-scale conflicts.
+
+			   Even the Gods and the Demons are 90% civilians. In addition, they are not bound by faith, so many "Small Gods", who in works like Discworld would be powerless, are instead of decent strength, just only within their Domain or Realm, their portfolio of powers. Only in Divine Blood would a character be the Goddess of Smiles. 
+
+			   Many supernatural races also have a bit of a twist to them, and are designed to be unique. Succubi, Kitsune, Troll, Dvergar...even Werewolves are not necessarially as you would know them, as myths and legends are often distortions of a truth. 
+
+			   Divine Blood is both a novel, which juggles High School Hijinks with Anime Action, a Tabletop Roleplaying game, designed to grant the players a chance to make their own stories and to learn more about their settings, and a series of short stories and collections, to show more of the setting. 
+			   
+			   We hope you enjoy Divine Blood, a novel available in paperback and Kindle, that is perfect for reading while waiting at the bus station, or at an airport terminal near you.
+		   </p>
+		    
+		</div>
+	</body>
+</html>
